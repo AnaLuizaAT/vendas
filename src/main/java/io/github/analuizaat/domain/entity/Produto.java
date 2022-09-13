cf2d4414246e9ae773624a0701c20d6b66944468
Ana Luiza Araujo <aluiza.araujot@gmail.com>
2022-09-12 23:18:44 -0400

    commit

--- src/main/java/io/github/analuizaat/domain/entity/Produto.java
+++ src/main/java/io/github/analuizaat/domain/entity/Produto.java
@@ -27,10 +27,10 @@ public class Produto {
     private Integer id;
 
     @Column(name = "descricao")
-    @NotEmpty(message = "Campo DESCRIÇÃO é obrigatório.")
+    @NotEmpty(message = "{campo.descricao.obrigatorio}")
     private String descricao;
 
     @Column(name = "preco_unitario")
-    @NotNull(message = "Campo PREÇO é obrigatório.")
+    @NotNull(message = "{campo.preco.obrigatorio}")
     private BigDecimal preco;
 }
