f00199282eb636cb6211ae3700e40b24c3144a37
Ana Luiza Araujo <aluiza.araujot@gmail.com>
2022-09-12 22:59:37 -0400

    commit

---
+++ src/main/java/io/github/analuizaat/validation/constraintValidation/NotEmptyListValidator.java
@@ -0,0 +1,17 @@
+package io.github.analuizaat.validation.constraintValidation;
+
+import io.github.analuizaat.validation.NotEmptyList;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+
+public class NotEmptyListValidator implements ConstraintValidator<NotEmptyList, List> {
+    @Override
+    public boolean isValid(List list, ConstraintValidatorContext constraintValidatorContext) {
+        return list != null && !list.isEmpty();
+    }
+
+    @Override
+    public void initialize(NotEmptyList constraintAnnotation) {}
+}
